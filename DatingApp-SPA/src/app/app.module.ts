cc25bc8863c09ced7e7fdbb3520c22a841306858
Zaid Qassim <zaidqassim12@gmail.com>
2020-03-11 22:51:37 +0300

    End of Seccsion 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,44 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from "ngx-bootstrap";
 
-import { HttpClientModule} from '@angular/common/http';
-import { FormsModule } from  '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap';
-
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { AuthService } from "./_services/auth.service";
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
+import { ErrorInterceptorProvider } from "./_services/error.interceptor";
+import { ListsComponent } from "./lists/lists.component";
+import { MemberListComponent } from "./member-list/member-list.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { appRoutes } from "./routes";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      FormsModule,
-      BsDropdownModule.forRoot()
-   ],
-   providers: [
-      AuthService,
-      ErrorInterceptorProvider
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    ListsComponent,
+    MemberListComponent,
+    MessagesComponent
+  ],
 
-   ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
+  ],
 
+  providers: [AuthService, ErrorInterceptorProvider],
 
-   bootstrap: [
-      AppComponent
-   ],
-
-   
-
-
-
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../_services/auth.service";
-import { AlertifyService } from '../_services/alertify.service';
- 
+import { AlertifyService } from "../_services/alertify.service";
+import { Router } from "@angular/router";
+
 @Component({
   selector: "app-nav",
   templateUrl: "./nav.component.html",
@@ -10,8 +11,11 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) {}
-  
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
 
@@ -22,6 +26,9 @@ export class NavComponent implements OnInit {
       },
       error => {
         this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(["/members"]);
       }
     );
   }
@@ -33,10 +40,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem("token");
     this.alertify.message("logged Out");
+    this.router.navigate(["/home"]);
   }
-
-
-
-
-  
 }
