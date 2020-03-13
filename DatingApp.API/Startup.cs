96dac0f7db1285d9df443a07d99a26b575d0d209
Zaid Qassim <zaidqassim12@gmail.com>
2020-03-13 07:09:18 +0300

    End of Secssion 8

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DatingApp.API.Data;
+﻿using DatingApp.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -19,6 +12,7 @@ using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using DatingApp.API.Helpers;
+using AutoMapper;
 
 namespace DatingApp.API
 {
@@ -37,9 +31,18 @@ namespace DatingApp.API
             // db connection string sql lite 
             services.AddDbContext<DataContext>(Options => Options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+            .AddJsonOptions(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling =
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
             services.AddCors();
+
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
+
             services.AddScoped<IAuthRepository, AuthRepoistory>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
@@ -66,10 +69,12 @@ namespace DatingApp.API
             }
             else
             {
-                app.UseExceptionHandler(bulilder => {
-                    bulilder.Run(async context => {
-                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
-                        var error  = context.Features.Get<IExceptionHandlerFeature>();
+                app.UseExceptionHandler(bulilder =>
+                {
+                    bulilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
                         if (error != null)
                         {
                             context.Response.AddApplicationError(error.Error.Message);
@@ -77,7 +82,6 @@ namespace DatingApp.API
                         }
                     });
                 });
-                // app.UseHsts();
             }
 
             // app.UseHttpsRedirection();
@@ -87,3 +91,4 @@ namespace DatingApp.API
         }
     }
 }
+
