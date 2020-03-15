b20e6b594a44679fd50f39e816d01ec99093f48d
Zaid Qassim <zaidqassim12@gmail.com>
2020-03-15 22:48:07 +0300

    End of Secction 9

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,21 @@
+import { Photo } from "./photo";
+
+export interface User {
+  /* for Dto  UserForlistDto   */
+  id: number;
+  userName: string;
+  knownAs: string;
+  age: number;
+  gender: number;
+  created: Date;
+  lastActive: Date;
+  photoUrl: string;
+  city: string;
+  country: string;
+
+  /* for Dto  UserForDetailedDto   */
+  interests?: string;
+  introduction?: string;
+  lookingFor?: string;
+  photos?: Photo[];
+}
